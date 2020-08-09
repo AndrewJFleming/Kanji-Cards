bfd85a789b33ee54d846f0decb936d568b4c5da1
AndrewJFleming <aflemi1@gmail.com>
2020-08-09 16:55:18 -0700

    Updates

---
+++ precache-manifest.7a729948d0edd08577edd6a385a785aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8aa094f30109c4857c043891528e71",
+    "url": "/kanji-cards/index.html"
+  },
+  {
+    "revision": "38c8244d01e2058c65c1",
+    "url": "/kanji-cards/static/css/main.c11a3829.chunk.css"
+  },
+  {
+    "revision": "33be89bc85360fd2c34a",
+    "url": "/kanji-cards/static/js/2.599abc96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kanji-cards/static/js/2.599abc96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38c8244d01e2058c65c1",
+    "url": "/kanji-cards/static/js/main.1d14843e.chunk.js"
+  },
+  {
+    "revision": "8afec26b9d536ab21318",
+    "url": "/kanji-cards/static/js/runtime-main.22681136.js"
+  },
+  {
+    "revision": "cdb1afa5420c1af730fcb227107a450c",
+    "url": "/kanji-cards/static/media/joanna-kosinska-1_CMoFsPfso-unsplash.cdb1afa5.jpg"
+  }
+]);
